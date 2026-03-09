046e1b69bbfa0401cf0cc14fe1d7b51a65c0059f
Maciej Flaga <maciejjjj4@gmail.com>
2026-03-09 16:48:07 +0100

    chyba dziala

--- z1/main.cpp
+++ z1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <complex>
+#include <fstream>
 
 using namespace std;
 typedef complex<double> cmp;
@@ -19,17 +20,24 @@ void psi_init(cmp **psi, int nx, double A, double sigma, double *x, double xc, d
     psi[nx][0]=0.0;
 }
 
-void thomas(cmp *nw, cmp *prawa, int n, cmp a, cmp b){
+void init_barrier(cmp *V, double *x, int nx, double Vmax){
+    for (int i=0;i<=nx;i++){
+        if (x[i]<= 0.2 && x[i]>=0.0) V[i]=Vmax;
+        else V[i]=0.0;
+    }
+}
+
+void thomas(cmp *nw, cmp *prawa, int n, cmp a, cmp *diag){
     cmp *cprim = new cmp[n-1];
     cmp *dprim = new cmp[n];
 
-    cprim[0]=a/b;
+    cprim[0]=a/diag[0];
     for (int i=1;i<n-1;i++){
-        cprim[i] = a/(b-a*cprim[i-1]);
+        cprim[i] = a/(diag[i]-a*cprim[i-1]);
     }
-    dprim[0]=prawa[0]/b;
+    dprim[0]=prawa[0]/diag[0];
     for (int i=1;i<n;i++){
-        dprim[i]=(prawa[i]-a*dprim[i-1])/(b-a*cprim[i-1]);
+        dprim[i]=(prawa[i]-a*dprim[i-1])/(diag[i]-a*cprim[i-1]);
     }
     nw[n-1]=dprim[n-1];
     for (int i=n-2;i>=0;i--){
@@ -48,17 +56,21 @@ void fillPrawa(cmp *prawa, int N, double dt, double theta, cmp **psi, int n, dou
 
 int main(){
     const double theta=0.5;
-    const double xmin=-1.89;
-    const double xmax=1.89;
+    const double xmin=-3.0;
+    const double xmax=3.0;
     const double tmax=1.0;
-    const int nx=100;
-    const int nt=100;
+    const int nx=150;
+    const int nt=2000;
     const double dx=(xmax-xmin)/nx;
     const double dt=tmax/nt;
-    const double xc=0.0;
+    const double xc=-1.0;
     const double p0=10.0;
-    const double A=1.758;
+    const double A=1.76;
     const double sigma=15.0;
+    const double Vmax=5.0;
+    const bool bar = true;
+    const int fps=30;
+    const int co_ktora=10;
     // alokacja
     cmp **psi = new cmp*[nx+1];
     for (int i=0;i<=nx;i++){
@@ -66,22 +78,49 @@ int main(){
     }
     cmp *V = new cmp[nx+1];
     double *x = new double[nx+1];
+    double *t = new double[nt+1];
     // inicjalizacja
-    for (int i=0;i<=nx;i++){
-        x[i] = xmin+i*dx;
-    }
-    for (int i=0;i<=nx;i++){
-        V[i]=0.0;
-    }
+    for (int i=0;i<=nx;i++) x[i] = xmin+i*dx;
+    if (bar) init_barrier(V,x,nx,Vmax);
+    else {for (int i=0;i<=nx;i++) V[i] = 0.0;}
+    for (int n=0;n<=nt;n++) t[n] = n*dt;
     psi_init(psi,nx,A,sigma,x,xc,p0,nt);
     // zmienne do pętli
     cmp *prawa = new cmp[nx-1];
     cmp *nw = new cmp[nx-1];
     fillPrawa(prawa,nx-1,dt,theta,psi,0,dx,V);
+    cmp *diag = new cmp[nx-1];
+    for (int i=0;i<nx-1;i++){
+        diag[i] = 2.0*dx*dx+2.0*iu*dt*theta+2.0*iu*dt*dx*dx*V[i+1]*theta;
+    }
     // pętla 
     for (int n=1;n<=nt;n++){
-        thomas(nw,prawa,nx-1,-iu*dt*theta,2.0*dx*dx+2.0*iu*dt*theta+2.0*iu*dt*dx*dx*V*theta) // diagonala będzie sie zmieniala razem z V więc trzeba zmodyfikować algorytm thomasa
+        thomas(nw,prawa,nx-1,-iu*dt*theta,diag);
+        for (int i=1;i<nx;i++) psi[i][n] = nw[i-1];
+        fillPrawa(prawa,nx-1,dt,theta,psi,n,dx,V);
     }
+    // zapis 
+    ofstream file("psi.dat");
+    for (int n=0;n<=nt;n++){
+        if (n%co_ktora==0) {for (int i=0;i<=nx;i++) file<<t[n]<<'\t'<<x[i]<<'\t'<<norm(psi[i][n])<<'\n';}
+    }
+    file.close();
+    // pliki pod przekaz danych
+    ofstream fpsfile("fps.dat");
+    fpsfile<<fps;
+    fpsfile.close();
+    ofstream misc("misc.dat");
+    misc<<
+    xmin<<'\n'<<
+    xmax<<'\n'<<
+    Vmax<<'\n'<<
+    bar<<'\n';
+    misc.close();
+    ofstream Vfile("V.dat");
+    for (int i=0;i<=nx;i++){
+        Vfile<<x[i]<<'\t'<<real(V[i])<<'\n';
+    }
+    Vfile.close();
     // czystki
     for (int i=0;i<=nx;i++){
         delete [] psi[i];
@@ -91,6 +130,8 @@ int main(){
     delete [] x;
     delete [] prawa;
     delete [] nw;
+    delete [] diag;
+    delete [] t;
     // return zero
     return 0;
 }
---
+++ z1/wykres.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+dane = np.loadtxt('psi.dat')
+misc = np.loadtxt('misc.dat')
+V = np.loadtxt('V.dat')
+
+xmin = misc[0]
+xmax = misc[1]
+Vmax = misc[2]
+bar = misc[3]
+
+tu = np.unique(dane[:,0])
+
+cwd = Path.cwd()
+frame_dir = cwd / "frames"
+frame_dir.mkdir(parents=True, exist_ok=True)
+
+plt.figure(figsize=(8,6))
+
+n=0
+psimax = max(dane[:,2])
+psimin = min(dane[:,2])
+
+if (bar):
+    ymax = 1.2*max(psimax,Vmax)
+else:
+    ymax = 1.2*psimax
+
+for t in tu:
+    mask = dane[:,0]==t
+    plt.plot(dane[mask,1],dane[mask,2])
+    #plt.tight_layout()
+    plt.title(f't={t:.3f}')
+    plt.axvline(x=xmin,ymin=psimin,ymax=psimax,color='black')
+    plt.axvline(x=xmax,ymin=psimin,ymax=psimax,color='black')
+    plt.plot(V[:,0],V[:,1],color='black')
+    plt.ylim(psimin,ymax)
+    plt.xlim(1.5*xmin,1.5*xmax)
+    plt.xlabel(rf'$x$')
+    plt.ylabel(rf'$|\Psi|^2$')
+    #plt.grid(ls=":")
+    filename = frame_dir / f"frame_{n:04d}.png"
+    n+=1
+    plt.savefig(filename)
+    plt.clf()
+plt.close()
